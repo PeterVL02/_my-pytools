d00b87ff08feea403d56375667be29fa1583681f
PeterVL02 <p.vesterenglarsen@gmail.com>
2026-05-19 02:58:04 +0200

    update project structure and dependencies; add self-dependency setup

--- src/_my_pytools/_setup/_setup_project.py
+++ src/_my_pytools/_setup/_setup_project.py
@@ -1,9 +1,11 @@
 from src._my_pytools._setup._setup_uv import setup_uv
+from src._my_pytools._setup._setup_self import add_self_dependency
 from src._my_pytools._setup._setup_beartype import init_beartype
 from src._my_pytools._maintenance._main_maintenance import main_maintenance
 
 def setup_project() -> None:
     """Initialize the project by setting up UV and beartype."""
     setup_uv()
+    add_self_dependency()
     init_beartype()
     main_maintenance()
---
+++ src/_my_pytools/_setup/_setup_self.py
@@ -0,0 +1,4 @@
+from src._my_pytools._setup._utils import add_dependency
+
+def add_self_dependency() -> None:
+    add_dependency('https://github.com/PeterVL02/_my-pytools.git')
